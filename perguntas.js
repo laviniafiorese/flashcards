80185d0d9c9fe27c73d52efdb4d1a0db40a01d3f
laviniafiorese <146454111+laviniafiorese@users.noreply.github.com>
2025-08-08 17:17:24 -0300

    ll

--- perguntas.js
+++ perguntas.js
@@ -5,13 +5,19 @@ criaCartao(
 )
 
 criaCartao(
-    'Liberdade de expressão',
-    'Onde termina a liberdade de expressão e começa o crime?',
-    'Quando suas palavras ameaçam, discriminam ou incitam ódio contra alguém.'
+    'Racismo',
+    'Se a escravidão acabou, por que o racismo continua?',
+    'Porque o racismo está nas estruturas da sociedade, não só nas leis.'
 )
 
 criaCartao(
     'Violência',
     'Quando uma mulher é morta por ser mulher, de quem é a culpa?',
     'Do agressor, e de uma sociedade que normaliza o machismo.'
+)
+
+criaCartao(
+    'Homofobia',
+    'Por que tanta gente se incomoda com o amor entre duas pessoas do mesmo sexo?',
+    'O preconceito vem da intolerância, da falta de respeito e da ideia de que só um tipo de amor é aceitável. '
 )
