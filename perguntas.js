563ccb16deba5fdc780d4a20a66803dd6f221540
laviniafiorese <146454111+laviniafiorese@users.noreply.github.com>
2025-08-07 21:50:52 -0300

    ll

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Desigualdade Social',
+    'Por que algumas pessoas nascem com tudo e outras com nada?',
+    'A desigualdade social cria oportunidades para uns e barreiras para outros, desde o nascimento.'
+)
+
+criaCartao(
+    'Liberdade de expressão',
+    'Onde termina a liberdade de expressão e começa o crime?',
+    'Quando suas palavras ameaçam, discriminam ou incitam ódio contra alguém.'
+)
+
+criaCartao(
+    'Violência',
+    'Quando uma mulher é morta por ser mulher, de quem é a culpa?',
+    'Do agressor, e de uma sociedade que normaliza o machismo.'
+)
